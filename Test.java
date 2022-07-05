f42c6be2e587817c10005903fe515d29395b5c74
kJosephCode <108654729+kJosephCode@users.noreply.github.com>
2022-07-04 23:51:35 -0700

    Update Test.java

--- Test.java
+++ Test.java
@@ -146,3 +146,121 @@ public class Test {
     // we only have to check if number is divisble by odd numbers.
 	}
 }
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class Tester {
+    public static void main(String[] args) throws IOException
+    {
+        int sortMaxSize = 16;
+        BufferedReader br = new  BufferedReader(new InputStreamReader(System.in)); // in order to read the entire line
+        System.out.println("Enter up to " + sortMaxSize + " elements with values between 1 and 9999 with one space between numbers: ");
+        int[] inputNums = new int[sortMaxSize];
+        String[] strArr;
+        strArr = br.readLine().split(" ");
+        // read user input till end of line
+        int count = strArr.length;
+        if (count > sortMaxSize) {
+            count = sortMaxSize;
+        }
+        for (int i=0; i<count; i++)
+        {
+            inputNums[i] = Integer.parseInt(strArr[i]);
+        }
+        // print array for debug
+        for (int i=0; i<count; i++) {
+            System.out.print(inputNums[i] + " ");
+        }
+        System.out.println();
+
+        // test1
+        
+        if (IsArrayPrimeIter(inputNums, count)) {
+            System.out.println("Array is prime");
+        } else {
+            System.out.println("Array is not prime");
+        }
+
+        // test2
+        if (IsArrayPrimeRecur(inputNums, count)) {
+            System.out.println("Array is prime");
+        } else {
+            System.out.println("Array is not prime");
+        }
+    }
+
+    public static boolean IsArrayPrimeIter(int[] arr, int size)
+    {
+        System.out.println("Entering IsArrayPrimeIter");
+        for (int i = 0; i < size; i++)
+        {
+            if (arr[i] == 1)
+            {
+                System.out.println("Leaving IsArrayPrimeIter");
+                return false; // 1 is not a prime number
+            }
+            if (arr[i] % 2 == 0)
+            {
+                System.out.println("Leaving IsArrayPrimeIter");
+                return false;
+            }
+
+            for (int j = 3; j <= Math.sqrt(arr[i])+1; j+=2)
+            {
+                if (arr[i] % j == 0)
+                {
+                    System.out.println("Leaving IsArrayPrimeIter");
+                    return false;
+                }
+            }
+        }
+        System.out.println("Leaving IsArrayPrimeIter");
+        return true;
+    }
+
+    public static boolean IsArrayPrimeRecur(int[] arr, int size)
+    {
+        System.out.println("Entering IsArrayPrimeRecur");
+        if (size == 0)
+        {
+            System.out.println("Leaving IsArrayPrimeRecur");
+            return true;
+        }
+        if (!IsPrimeRecur(arr[size-1], 3)) // check if the last element in the array is prime
+        {
+            System.out.println("Leaving IsArrayPrimeRecur");
+            return false;
+        }
+        System.out.println("Leaving IsArrayPrimeRecur");
+        return IsArrayPrimeRecur(arr, size-1);
+    }
+
+    public static boolean IsPrimeRecur(int checkNum, int divisorNum)
+    {
+        System.out.println("Entering IsPrimeRecur");
+        if (checkNum == 1)
+        {
+            System.out.println("Leaving IsPrimeRecur");
+            return false;
+        }
+        if (divisorNum > Math.sqrt(checkNum) + 1)
+        {
+            System.out.println("Leaving IsPrimeRecur");
+            return true;
+        }
+        if (checkNum % 2 == 0 || checkNum % divisorNum ==0)
+        {
+            System.out.println("Leaving IsPrimeRecur");
+            return false;
+        }
+        System.out.println("Leaving IsPrimeRecur");
+        return IsPrimeRecur(checkNum, divisorNum+2);
+    }
+}
+
+
